4235de7e9f5d07c6df990a38df53831d724432c2
Mart Kaasik <mart.kaasik@gmail.com>
2020-08-15 18:31:17 +0300

    Updates

---
+++ precache-manifest.033dc2990665c6f3f48cd50a30aa42b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02779f5c595cdc31e57df642199795ff",
+    "url": "/Project-Antelope/index.html"
+  },
+  {
+    "revision": "95a70fa78ea2a3007077",
+    "url": "/Project-Antelope/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34bfd3666bd58369a647",
+    "url": "/Project-Antelope/static/js/2.2b772e54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project-Antelope/static/js/2.2b772e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a70fa78ea2a3007077",
+    "url": "/Project-Antelope/static/js/main.67eec009.chunk.js"
+  },
+  {
+    "revision": "62f185e75bd7722e1f1a",
+    "url": "/Project-Antelope/static/js/runtime-main.a50bbd62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project-Antelope/static/media/logo.5d5d9eef.svg"
+  }
+]);
